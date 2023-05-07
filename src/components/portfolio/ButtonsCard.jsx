a1457f1a03e7a19f5c484ff4f02e8ce818e9c01f
julio <julioroja987@gmail.com>
2023-05-07 18:58:10 -0300

    Se hace clickeable los textos de los botones de la card

--- src/components/portfolio/ButtonsCard.jsx
+++ src/components/portfolio/ButtonsCard.jsx
@@ -5,15 +5,15 @@ import { RiGithubFill, RiExternalLinkLine } from "react-icons/ri";
 const ButtonsCard = ({ web, github }) => {
     return (
         <div className="flex gap-2 justify-center font font-semibold text-2xl">
-            <div className="flex gap-1">
-                <span className="text-base">GitHub</span>
-                <Link href={github} target="_blank">
+            <div>
+                <Link href={github} target="_blank" className="flex gap-1">
+                    <span className="text-base">GitHub</span>
                     <RiGithubFill />
                 </Link>
             </div>
-            <div className="flex gap-1">
-                <span className="text-base">Demo</span>
-                <Link href={web} target="_blank">
+            <div>
+                <Link href={web} target="_blank" className="flex gap-1">
+                    <span className="text-base">Demo</span>
                     <RiExternalLinkLine />
                 </Link>
             </div>
