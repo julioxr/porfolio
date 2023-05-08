19ccbccb98d548e35db804b9f4c9f97efd519e25
julio <julioroja987@gmail.com>
2023-05-08 11:09:21 -0300

    Se corrige el analytics

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata = {
@@ -11,6 +12,19 @@ export default function RootLayout({ children }) {
         <html lang="es" className="scroll-smooth">
             <Head>
                 <link rel="icon" href="/julioLogo.png" />
+                <Script
+                    async
+                    src="https://www.googletagmanager.com/gtag/js?id=G-60T6ES4N7Z"
+                ></Script>
+                <Script>
+                    {`
+                    window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+
+                        gtag('config', 'G-60T6ES4N7Z');
+                    `}
+                </Script>
             </Head>
             <body className="text-night overflow-x-hidden">
                 <main>{children}</main>
--- src/components/header/MainHeader.jsx
+++ src/components/header/MainHeader.jsx
@@ -1,11 +1,7 @@
 "use client";
 import { useState } from "react";
-import ReactGA from "react-ga";
 import MainMenuHeader from "@/components/header/MainMenuHeader";
 import BurgerMenuHeader from "@/components/header/BurgerMenuHeader";
-
-const TRACKING_ID = "G-60T6ES4N7Z";
-ReactGA.initialize(TRACKING_ID);
 
 const MainHeader = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
